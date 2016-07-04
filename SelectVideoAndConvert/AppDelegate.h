7071171b0262ba39d723066a540ddeb380272a8f
GE_CHENGMING <984603904@qq.com>
2016-07-04 17:12:14 +0800

    Initial Commit

---
+++ SelectVideoAndConvert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectVideoAndConvert
+//
+//  Created by macavilang on 16/7/4.
+//  Copyright © 2016年 Snoopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectVideoAndConvert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectVideoAndConvert
+//
+//  Created by macavilang on 16/7/4.
+//  Copyright © 2016年 Snoopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
